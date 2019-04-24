a5e6960ba740f4eda1c72132768ed6628cf24c63
Francisco Peters <francisco.peters91@gmail.com>
2019-04-23 21:19:23 -0400

    features
    added progress bar and separate submits for each question.

--- js/quiz.js
+++ js/quiz.js
@@ -1,103 +1,152 @@
-function generateQuiz(questions, quizContainer, resultsContainer, submitButton){
+function generateQuiz(questions, quizContainer, resultsContainer, submitButton, nextButton){
 
-	function showQuestions(questions, quizContainer){
-	// we'll need a place to store the output and the answer choices
-	var output = [];
-	var answers;
-
-	// for each question...
-	for(var i=0; i<questions.length; i++){
-		
-		// first reset the list of answers
-		answers = [];
+	function showQuestion(numQuestion, questions, quizContainer) {
+		// we'll need a place to store the output and the answer choices
+		var output = [];
+		var answers = [];
 
 		// for each available answer to this question...
-		for(letter in questions[i].answers){
-
+		for(letter in questions[numQuestion].answers){		
 			// ...add an html radio button
 			answers.push(
 				'<label>'
-					+ '<input type="radio" name="question'+i+'" value="'+letter+'">'
-					+ questions[i].answers[letter]
+					+ '<input type="radio" name="question'+numQuestion+'" value="'+letter+'">'
+					+ questions[numQuestion].answers[letter]
 				+ '</label></br>'
 			);
 		}
 
 		// add this question and its answers to the output
 		output.push(
-			'<div class="question">' + questions[i].question + '</div>'
+			'<div class="question">' + questions[numQuestion].question + '</div>'
 			+ '<div class="answers">' + answers.join('') + '</div>'
 		);
+
+		// finally combine our output list into one string of html and put it on the page
+		quizContainer.innerHTML = output.join('');
 	}
 
-	// finally combine our output list into one string of html and put it on the page
-	quizContainer.innerHTML = output.join('');
-	}
-
-	function showResults(questions, quizContainer, resultsContainer){
+	function showResult(numQuestion, questions, quizContainer, resultsContainer) {
 		// gather answer containers from our quiz
 		var answerContainers = quizContainer.querySelectorAll('.answers');
 		// keep track of user's answers
 		var userAnswer = '';
-		var numCorrect = 0;
+		// find selected answer
+		userAnswer = (answerContainers[0].querySelector('input[name=question'+numQuestion+']:checked')||{}).value;
 		
-		// for each question...
-		for(var i=0; i<questions.length; i++){
-
-			// find selected answer
-			userAnswer = (answerContainers[i].querySelector('input[name=question'+i+']:checked')||{}).value;
-			
-			// if answer is correct
-			if(userAnswer===questions[i].correctAnswer){
-				// add to the number of correct answers
-				numCorrect++;
-				
-				// color the answers green
-				answerContainers[i].style.color = 'lightgreen';
-			}
-			// if answer is wrong or blank
-			else{
-				// color the answers red
-				answerContainers[i].style.color = 'red';
-			}
+		// if answer is correct
+		if(userAnswer===questions[numQuestion].correctAnswer){
+			// add to the number of correct answers
+			correctAnswers++;
+			// color the answers green
+			answerContainers[0].style.color = 'lightgreen';
 		}
-
-		// show number of correct answers out of total
-		resultsContainer.innerHTML = numCorrect + ' out of ' + questions.length;
+		// if answer is wrong or blank
+		else{
+			// color the answers red
+			answerContainers[0].style.color = 'red';
+		}
 	}
 
-	// show the questions
-	showQuestions(questions, quizContainer);
+	function showEndResult(questions, correctAnswers, resultsContainer) {
+		// show number of correct answers out of total
+		resultsContainer.innerHTML = 'You got ' + correctAnswers + ' out of ' + questions.length + ' answers right.';
+	}
 
-	// when user clicks submit, show results
-	submitButton.onclick = function(){
-		showResults(questions, quizContainer, resultsContainer);
+	// functions to update the progress bar
+
+	// add progress to the progress bar
+	function addProgress() {
+	    //increase myBarWidth by 100/questionsNumber
+	    myBar.innerHTML = '<center style="color:white">'+noQuestion+'/'+numberQuestions+'</center>';
+	    myBarWidth += 100 / numberQuestions;
+	    // make sure that width of the progress bar won't be more than 100% & fix for questionsNumber that are not dividers of 100
+	    if (myBarWidth > 100) {
+	        myBarWidth = 100;
+	    }
+	    // update the width #myBar by changing the css
+	    document.getElementById("myBar").style.width = myBarWidth + "%";
+	}
+
+	// deduct progress
+	function deductProgress() {
+	    //decrease myBarWidth by 100/questionsNumber
+	    myBarWidth -= 100 / myQuestions.length;
+	    // make sure that width of the progress bar won't be more than 0% & fix for questionsNumber that are not dividers of 100
+	    if (myBarWidth < 0) {
+	        myBarWidth = 0;
+	    }
+	    // update the width #myBar by changing the css
+	    document.getElementById("myBar").style.width = myBarWidth + "%";
+	}
+
+	var noQuestion = 0;
+	var correctAnswers = 0;
+	var numberQuestions = questions.length;
+	var myBarWidth = 0;
+	myBar.innerHTML = '<center style="color:white">'+noQuestion+'/'+numberQuestions+'</center>';
+
+
+	// show the questions
+	showQuestion(noQuestion, questions, quizContainer);
+	nextButton.style.visibility = "hidden"; 
+
+	submitButton.onclick = function() {
+		showResult(noQuestion, questions, quizContainer, resultsContainer, correctAnswers);
+		submitButton.style.visibility = "hidden";
+		// condition for end of the quiz
+		if (noQuestion >= questions.length - 1) {
+			showEndResult(questions, correctAnswers, resultsContainer);
+			nextButton.style.visibility = "hidden"; 
+		}
+		else {
+			nextButton.style.visibility = "visible";
+		}
+		noQuestion++;
+		addProgress();
+	}
+
+	nextButton.onclick = function() {
+		showQuestion(noQuestion, questions, quizContainer);
+		submitButton.style.visibility = "visible"; 
+		nextButton.style.visibility = "hidden"; 
 	}
 }
 
 var myQuestions = [
 	{
-		question: "What is 10/2?",
+		question: "What is the real name of Doctor Eggman?",
 		answers: {
-			a: '3',
-			b: '5',
-			c: '115'
+			a: 'Machigero',
+			b: 'Robotnik',
+			c: 'Machinarium'
 		},
 		correctAnswer: 'b'
 	},
 	{
-		question: "What is 30/3?",
+		question: "What character was the first Sonic's sidekick?",
 		answers: {
-			a: '3',
-			b: '5',
-			c: '10'
+			a: 'Knuckles',
+			b: 'Amy',
+			c: 'Tails'
 		},
 		correctAnswer: 'c'
+	},
+	{
+		question: "In what game does Shadow appear for the first time?",
+		answers: {
+			a: 'Sonic Adventure',
+			b: 'Sonic Adventure 2',
+			c: 'Sonic Heroes'
+		},
+		correctAnswer: 'b'
 	}
 ];
 
 var quizContainer = document.getElementById('quiz');
 var resultsContainer = document.getElementById('results');
 var submitButton = document.getElementById('submit');
+var nextButton = document.getElementById('next');
+var questionProgress = document.getElementById('questionProgress');
 
-generateQuiz(myQuestions, quizContainer, resultsContainer, submitButton);
+generateQuiz(myQuestions, quizContainer, resultsContainer, submitButton, nextButton, questionProgress);
